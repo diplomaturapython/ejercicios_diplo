6be9184c3b812c45db3cba4a3e0c78770544b7b6
diplomaturapython <labseguinvestigacion@gmail.com>
2025-04-05 11:48:11 -0300

    Add files via upload

---
+++ for.py
@@ -0,0 +1,23 @@
+#for variable in secuencia
+'''
+frutas=["manzana","banana","naranja"]
+
+for fruta in frutas:
+    print(fruta)
+
+#for num in range(10):
+ #   print(num, end=", ")
+  #  .
+
+texto="diplomatura"
+vocal="aeiou"
+contador = 0
+for letra in texto:
+    if letra in vocal:
+        contador +=1
+print(contador)
+
+'''
+for numero in range(21):
+    if numero %2 == 0:
+        print(numero, end=" ")
