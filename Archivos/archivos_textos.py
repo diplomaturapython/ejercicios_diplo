35c9c28f265072185a87c368faea411113ffd5de
diplomaturapython <labseguinvestigacion@gmail.com>
2025-05-07 13:08:07 -0300

    07-05-2025

--- Archivos/archivos_textos.py
+++ Archivos/archivos_textos.py
@@ -1,4 +1,15 @@
-archivo = open("lenguajes.txt", "r", encoding="utf-8")
+#Para abrir el archivo "lenguaje.txt" hacer lo siguiente:
+archivo = open("Direccion donde esta el archivo", "r")
 contenido = archivo.read()
 print(contenido)
 archivo.close()  #
+
+#Rutina para escribir un archivo
+archivo = open("nombre_archivo.txt", "w")
+archivo.write("Texto de ejemplo\n")
+archivo.close()
+
+#Rutina para escribir el archivo al final de linea sin borrar lo anterior
+archivo = open("nombre_archivo.txt", "a")# en vez de "w" se pone "a" que es para agregar al final del archivo a= append
+archivo.write("Texto de ejemplo\n")
+archivo.close()
